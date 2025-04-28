39458571e7b65bf259ee34f37c772f20fe882c65
NybiLin <brandon.picq@gmail.com>
2025-04-28 13:43:54 +0200

    Addition to the javascript

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 const submitBtn = document.getElementById("submit-btn");
 const popupThank = document.getElementById("message-thanks");
+let formGroup = document.querySelectorAll(".form-group");
 
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
   popupThank.style.display = "flex";
   submitBtn.style.display = "none";
+  formGroup.forEach((input) => {
+    input.style.display = "none";
+  });
 });
